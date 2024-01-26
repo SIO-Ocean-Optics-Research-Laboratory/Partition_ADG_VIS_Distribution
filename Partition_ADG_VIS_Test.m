95381a1d204b8e452841e00f3480c064c459869e
Matthew Kehrli <61636816+mdkehrli@users.noreply.github.com>
2024-01-26 15:12:42 -0800

    Update Partition_ADG_VIS_Test.m

--- Partition_ADG_VIS_Test.m
+++ Partition_ADG_VIS_Test.m
@@ -11,7 +11,7 @@
 %Kehrli, M. D., Stramski, D., Reynolds, R. A., Joshi, I. D., A model for
 %partitioning the non-phytoplankton absorption coefficient of seawater in the
 %ultraviolet and visible spectral range into the contribution of non-algal
-%particulate and dissolved matter. Submitted to Applied Optics January 24, 
+%particulate and dissolved organic matter. Submitted to Applied Optics January 24, 
 %2024 (Referenced in code documentation as KSRJ).
 %
 %Created: July 31, 2023
